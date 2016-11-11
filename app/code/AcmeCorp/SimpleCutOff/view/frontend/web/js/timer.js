362fdaac9667100be719a79a4072ff418d04d4b6
thomasraskob <thoamsraskob444@gmail.com>
2016-11-11 22:17:47 +0800

    complete version

--- app/code/AcmeCorp/SimpleCutOff/view/frontend/web/js/timer.js
+++ app/code/AcmeCorp/SimpleCutOff/view/frontend/web/js/timer.js
@@ -16,7 +16,7 @@ define([
             this.endTime = new Date(this.options.endTimeString);
 
             this.tickCount = 0;
-            this.intervalId = setInterval(this._tick.bind(this), 1000);
+            this.intervalId = setInterval(this._tick.bind(this), 1000 * 30);
         },
 
         _tick: function() {
@@ -25,15 +25,8 @@ define([
                 var diff = this.endTime - now;
                 var h = parseInt(diff / (3600 * 1000));
                 var m = parseInt(diff / (60*1000)) - h * 60;
-                var s = parseInt(diff / 1000) - h * 3600 - m * 60;
-                jQuery(this.element).find("#remaining-time").text(h + "Hrs " + m + "Mins " + s + "Secs");
+                jQuery(this.element).find("#remaining-time").text(h + "Hrs " + m + "Mins ");
             } else {
-                // jQuery(this.element).text("ENDED");
-                // clearInterval(this.intervalId);
-            }
-
-
-            if (this.tickCount++ == 100) {
                 jQuery(this.element).text("ENDED");
                 clearInterval(this.intervalId);
             }
